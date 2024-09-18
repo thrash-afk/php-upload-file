7c26883ce68fead710649d2bc4c2a6e6b628a613
Ilia <kidrock1@yandex.ru>
2024-09-18 22:04:06 +0700

    Add comments

--- index.php
+++ index.php
@@ -3,19 +3,23 @@
 	header("Access-Control-Allow-Origin: *");
 	header("Access-Control-Allow-Method: POST");
 	
+	# Если метод GET, выкидываем заглушку
 	if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 		echo "Ooops!!!";
 	}
+	
+	# Если метод POST и есть файлы в запросе
 	else if ($_SERVER['REQUEST_METHOD'] == 'POST' and sizeof($_FILES) > 0) {
 	 $data = json_decode(file_get_contents("php://input"), true);
 	
-		echo "Массив " . sizeof($_FILES['file']) . "\n";
+		echo "Массив " . sizeof($_FILES['file']) . "\n";	# удалить
 	
-		$countfiles = count($_FILES['file']['name']);
-		echo "Всего файлов " . $countfiles . "\n";
-		echo "POST!\n";
-		print_r($_FILES);
+		$countfiles = count($_FILES['file']['name']);		# удалить
+		echo "Всего файлов " . $countfiles . "\n";			# удалить
+		echo "POST!\n";										# удалить
+		print_r($_FILES);									# удалить
 	}
+	
 	else {
 		echo json_encode(array("message" => "Что-то пошло не так", "status" => "false"));
 	}
